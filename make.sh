7820ab7983c218e11dce793bd04b4114feae9e3a
mkosem <mattkosem@gmail.com>
2019-09-02 18:14:03 -0400

    adding stuff

---
+++ make.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+ST_VERSION=st-0.8.2
+rm -rf $ST_VERSION
+tar -xvf $ST_VERSION.tar.gz
+pushd $ST_VERSION
+patch < ../st-scrollback-0.8.2.diff
+patch < ../st-scrollback-mouse-0.8.2.diff
+patch < ../st-scrollback-mouse-altscreen-0.8.diff
+cp ../config.h .
+make
+popd
+
+
+
+
